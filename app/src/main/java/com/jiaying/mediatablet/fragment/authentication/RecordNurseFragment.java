c76bf05f902f9589327136da626861d23c6e95a6
libo <353510746@qq.com>
2016-08-12 10:10:08 +0800

    1.调整录制浆员视频和录制护士视频的录制按钮的位置

--- app/src/main/java/com/jiaying/mediatablet/fragment/authentication/RecordNurseFragment.java
+++ app/src/main/java/com/jiaying/mediatablet/fragment/authentication/RecordNurseFragment.java
@@ -98,7 +98,7 @@ public class RecordNurseFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_record_donor_video, container, false);
+        View view = inflater.inflate(R.layout.fragment_record_nurse_video, container, false);
 
         fdActivity = new FdNotAuthRecordActivity(this, 1);
         fdActivity.onCreate(view);
